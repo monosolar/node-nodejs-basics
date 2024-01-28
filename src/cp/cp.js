429fe854c448caebb1d9fd16e714923149f067c7
ekosenko <evgeni.kosenko@mobisystems.com>
2024-01-28 22:26:14 +0300

    CP

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,26 @@
+import { spawn } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = spawn(
+    'node',
+    [path.resolve(__dirname, './files/script.js'), ...args],
+    {
+      stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
+    },
+  );
+
+  child.on('exit', (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(/* [someArgument1, someArgument2, ...] */);
