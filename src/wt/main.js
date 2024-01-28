ae1b6f3848459701b366df69c0c78bbda5b7d7d7
ekosenko <evgeni.kosenko@mobisystems.com>
2024-01-28 22:06:52 +0300

    WT, Zlib

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,37 @@
+import { Worker } from 'worker_threads';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { cpus } from 'os';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const createWorker = (idx) =>
+  new Promise((res, rej) => {
+    const worker = new Worker(path.resolve(__dirname, './worker.js'), {
+      workerData: 10 + idx,
+    });
+    worker.on('message', res);
+    worker.on('error', rej);
+  });
+
 const performCalculations = async () => {
-    // Write your code here
+  const numCPUs = cpus().length;
+
+  const rawResults = await Promise.allSettled(
+    new Array(numCPUs).fill().map((_, idx) => createWorker(idx + 1)),
+  );
+
+  const results = rawResults.map(({ status, value: data }) => {
+    const isFulfilled = status === 'fulfilled';
+
+    return {
+      status: isFulfilled ? 'resolved' : 'error',
+      data: isFulfilled ? data : null,
+    };
+  });
+
+  console.log(results);
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,15 @@
 // n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { workerData, parentPort } from 'worker_threads';
+
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  if (Math.random() > 0.5) {
+    parentPort.postMessage(nthFibonacci(workerData));
+  } else {
+    throw new Error('Simulated error in worker thread');
+  }
 };
 
 sendResult();
